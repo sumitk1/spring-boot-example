b1e637dd089d9d0d744f8981d680569ab9f56cdb
SumitKumar <sumitk.85@gmail.com>
2015-04-13 11:37:32 -0700

    Adding sample health endpoint

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,4 +1,3 @@
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +16,13 @@ public class Example {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Example.class, args);
+    @RequestMapping("/health1")
+    String home1() {
+        return "Ffffffff!";
     }
+
+   /* public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }*/
 
 }
